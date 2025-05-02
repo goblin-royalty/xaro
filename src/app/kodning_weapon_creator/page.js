2b014f96b6438f913eb8576644b1b7aa5b4dfbb7
VictorNegridemos <gorqshtjiraf11@abv.bg>
2025-05-02 04:31:51 +0300

    weapon creator

---
+++ src/app/kodning_weapon_creator/page.js
@@ -0,0 +1,10 @@
+import Main from "../../components/Main/Main";
+import WeaponCreator from "../../components/WeaponCreator/WeaponCreator";
+
+export default async function KodningWeaponCreator() {
+    return (
+        <Main>
+            <WeaponCreator/>
+        </Main>
+    );
+}
